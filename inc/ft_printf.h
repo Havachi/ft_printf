2939055f530a596fc640135a3fdda96f87ab5e1e
Havachi <havachi@pop-os.localdomain>
2021-10-28 09:34:10 +0200

    prochaine arret lausanne

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -1,4 +1,20 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <stdlib.h>
+typedef struct	s_format
+{
+	char *type;
+	union
+	{
+		char		c;
+		char		*s;
+		void		*vptr;
+		int		d;
+		int		i;
+		unsigned int 	u;
+		unsigned int 	x;
+	}
+} t_format; 
 int	ft_printf(const char *, ...);
+t_format	*parse_format(const char *s);
 #endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,5 +1,45 @@
+#include "ft_printf.h"
+
 int	ft_printf(const char *s, ...)
 {
-	(void)s;
+	t_format	*formats;
+	formats = parse_format(s);
 	return (0);
+}
+
+t_format	*parse_format(const char *s)
+{
+	t_format	*formats;
+	int		i;
+	int		nbformat;
+
+	i = 0;
+	nbformat = count_format(s);
+	if (!s)
+		return (NULL);
+	
+	while(s[i] != '\0')
+	{
+		if(s[i] = '%')
+	}
+}
+
+int		count_format(const char *s)
+{
+	int	i;
+	int	nbformat;
+	
+	i = 0;
+	nbformat = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] = '%')
+		{	
+			if(s[i+1] = '%')
+				i++;
+			nbformat++;
+		}
+		i++;
+	}
+	return (nbformat);
 }
